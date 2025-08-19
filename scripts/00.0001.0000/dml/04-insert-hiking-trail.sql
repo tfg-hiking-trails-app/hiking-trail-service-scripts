031024a855e9b214810c7d95338c3e4400f78050
RubenFern <rubenferval@hotmail.com>
2025-08-20 01:22:19 +0200

    new test data

--- scripts/00.0001.0000/dml/04-insert-hiking-trail.sql
+++ scripts/00.0001.0000/dml/04-insert-hiking-trail.sql
@@ -3,4 +3,8 @@ VALUES
     ('e0533df9-1c00-4308-a1fe-edb7ab0215d2', 1, 1, 1, 'Easy Trail', 'A nice and easy trail for beginners.', true, '2025-08-03 12:00:00', '2025-08-03 14:30:00', 40.7128, -74.0060),
     ('e0533df9-1c00-4308-a1fe-edb7ab0215d2', 2, 2, 2, 'Medium Trail', 'A moderately challenging trail.', true, '2025-08-03 09:00:00', '2025-08-03 15:00:00', 34.0522, -118.2437),
     ('e0533df9-1c00-4308-a1fe-edb7ab0215d2', 3, 3, 3, 'Hard Trail', 'A hard trail for experienced hikers.', false, '2025-08-03 07:00:00', '2025-08-03 17:00:00', 51.5074, -0.1278),
-    ('06d1d7d1-9c32-4d15-9765-0c3692409fe3', 4, 4, 1, 'Very Hard Trail', 'A very hard trail for expert hikers only.', false, '2025-08-03 06:00:00', '2025-08-03 18:00:00', 48.8566, 2.3522);
+    ('06d1d7d1-9c32-4d15-9765-0c3692409fe3', 4, 4, 1, 'Very Hard Trail4', 'A very hard trail for expert hikers only.', false, '2025-08-04 06:00:00', '2025-08-04 18:00:00', 48.8566, 2.3522),
+    ('06d1d7d1-9c32-4d15-9765-0c3692409fe3', 4, 4, 1, 'Very Hard Trail1', 'A very hard trail for expert hikers only.', false, '2025-08-01 06:00:00', '2025-08-01 18:00:00', 48.8566, 2.3522),
+    ('06d1d7d1-9c32-4d15-9765-0c3692409fe3', 4, 4, 1, 'Very Hard Trail17', 'A very hard trail for expert hikers only.', false, '2025-08-17 06:00:00', '2025-08-17 18:00:00', 48.8566, 2.3522),
+    ('06d1d7d1-9c32-4d15-9765-0c3692409fe3', 4, 4, 1, 'Very Hard Trail29', 'A very hard trail for expert hikers only.', false, '2025-08-29 06:00:00', '2025-08-29 18:00:00', 48.8566, 2.3522),
+    ('06d1d7d1-9c32-4d15-9765-0c3692409fe3', 4, 4, 1, 'Very Hard Trail5', 'A very hard trail for expert hikers only.', false, '2025-08-05 06:00:00', '2025-08-05 18:00:00', 48.8566, 2.3522);
