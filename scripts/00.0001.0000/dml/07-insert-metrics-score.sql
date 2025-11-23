7080a889919aaac55d4d237af85cea34f3e79a72
RubenFern <rubenferval@hotmail.com>
2025-11-23 16:31:13 +0100

    new insert

---
+++ scripts/00.0001.0000/dml/07-insert-metrics-score.sql
@@ -0,0 +1,3 @@
+INSERT INTO MetricsScore (account_code, distance, duration, steps, calories, pace, elevation, heart_rate, speed)
+VALUES
+    ('e0533df9-1c00-4308-a1fe-edb7ab0215d2', 8, 5, 8, 3, 2, 7, 9, 5);
